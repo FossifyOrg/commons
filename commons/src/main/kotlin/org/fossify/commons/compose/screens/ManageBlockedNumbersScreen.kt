92abc634f57ba229d8652c4c0ef8a943e6f0ee20
Agnieszka C <85929121+Aga-C@users.noreply.github.com>
2025-08-10 10:40:53 +0200

    Removed old XML layouts

--- commons/src/main/kotlin/org/fossify/commons/compose/screens/ManageBlockedNumbersScreen.kt
+++ commons/src/main/kotlin/org/fossify/commons/compose/screens/ManageBlockedNumbersScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ContentCopy
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
