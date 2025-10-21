06821931276e7668ef44f58629ca4a9342784af3
Naveen Singh <snaveen935@gmail.com>
2025-10-21 18:47:51 +0530

    feat: add helpers for system bars

--- commons/src/main/kotlin/org/fossify/commons/extensions/Window.kt
+++ commons/src/main/kotlin/org/fossify/commons/extensions/Window.kt
@@ -1,13 +1,33 @@
 package org.fossify.commons.extensions
 
 import android.view.Window
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import org.fossify.commons.helpers.DARK_GREY
 
+fun Window.insetsController(): WindowInsetsControllerCompat {
+    return WindowInsetsControllerCompat(this, decorView)
+}
+
 fun Window.setSystemBarsAppearance(backgroundColor: Int) {
     val isLightBackground = backgroundColor.getContrastColor() == DARK_GREY
-    with(WindowInsetsControllerCompat(this, decorView)) {
+    with(insetsController()) {
         isAppearanceLightStatusBars = isLightBackground
         isAppearanceLightNavigationBars = isLightBackground
+    }
+}
+
+fun Window.showBars() {
+    with(insetsController()) {
+        show(WindowInsetsCompat.Type.systemBars())
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
+    }
+}
+
+fun Window.hideBarsTransient() {
+    with(insetsController()) {
+        hide(WindowInsetsCompat.Type.systemBars())
+        systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
