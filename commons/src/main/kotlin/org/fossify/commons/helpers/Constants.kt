fbfc32d2b53a4dc00795d3c71ecc538e5ec71763
Naveen Singh <snaveen935@gmail.com>
2025-10-24 21:43:10 +0530

    feat: add default animation duration constant

--- commons/src/main/kotlin/org/fossify/commons/helpers/Constants.kt
+++ commons/src/main/kotlin/org/fossify/commons/helpers/Constants.kt
@@ -50,6 +50,7 @@ const val MD5 = "MD5"
 const val SHA1 = "SHA-1"
 const val SHA256 = "SHA-256"
 const val SHORT_ANIMATION_DURATION = 150L
+const val DEFAULT_ANIMATION_DURATION = 300L
 val DARK_GREY = 0xFF333333.toInt()
 
 const val LOWER_ALPHA = 0.25f
